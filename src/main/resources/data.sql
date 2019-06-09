331b289424e8525f0d5e7091f518d18e6106dabc
omvikas <er.omvikas@gmail.com>
2019-06-10 01:56:09 +0530

    Adding BcryptEncoder

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-Insert INTO user (user_id,active,email,last_name,fname,password) values (1,1,'om@example.com','mishra','om','123');
+Insert INTO user (user_id,active,email,last_name,fname,password) values (1,1,'om@example.com','mishra','om','$2a$10$iEyZWvIb3nhunhiOn7Zo3uUiIjrYAXWMwdEbjoJbmTf4GCnYXxXM2');
 Insert INTO role values (1,'Admin');
 Insert INTO user_role values (1,1);
